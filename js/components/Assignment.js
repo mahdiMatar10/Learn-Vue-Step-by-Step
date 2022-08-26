a301e56f2e8e53f4f7459589fa6f8d8f8e2d83d2
mahdiMatar10 <mahdi.matar.1999@gmail.com>
2022-08-26 11:57:32 +0300

    add flex between label and checkbox input, margin left

--- js/components/Assignment.js
+++ js/components/Assignment.js
@@ -2,10 +2,10 @@ export default {
   template: `
          
        <li>
-    <label class="p-2">
+    <label class="p-2 flex justify-between item-center">
         {{ assignment.name }}
 
-          <input type="checkbox" v-model="assignment.complete">
+          <input type="checkbox" v-model="assignment.complete" class="ml-3">
     </label>
   </li>
         `,
